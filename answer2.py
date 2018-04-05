e5ced6646a472e4d010523af6e4c45ba2e13277b
kapilarj <37668047+kapilarj@users.noreply.github.com>
2018-04-05 19:33:03 +0530

    Add files via upload

---
+++ answer2.py
@@ -0,0 +1,31 @@
+# cook your dish here
+
+def fact(n):
+    if n==1 or n==0:
+        return n
+    else:
+        return n* fact(n-1)
+
+n = int(input())
+
+list = [];
+for i in range(n):
+    list.append(input())
+list = sorted(list)
+
+x = input()
+
+y = n
+count = 1
+n=n-1
+
+for i in range(y):
+    id = list.index(x[i])
+    list.remove(x[i])
+    if id != 0 :
+        count = count + (id) * fact(n)
+        
+    n = n-1
+
+print(count)
+
